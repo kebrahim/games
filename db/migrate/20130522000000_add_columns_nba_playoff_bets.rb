0a54499a5ad2b3f7655fcac9865901e4381ffabf
Kurt Ebrahim <kebrahim@gmail.com>
2013-06-08 19:23:29 -0400

    fixed migration scripts

--- db/migrate/20130522000000_add_columns_nba_playoff_bets.rb
+++ db/migrate/20130522000000_add_columns_nba_playoff_bets.rb
@@ -2,13 +2,14 @@ class AddColumnsNbaPlayoffBets < ActiveRecord::Migration
   def self.up
     add_column :nba_playoff_bets, :round, :integer
     add_column :nba_playoff_bets, :position, :integer
-    add_index :nba_playoff_bets, [:year, :round, :position], :unique => true
+    add_index :nba_playoff_bets, [:year, :round, :position],
+        {:unique => true, :name => "nba_playoff_bets_year_round_position"}
     remove_index :nba_playoff_bets, :nba_playoff_matchup_id
     remove_column :nba_playoff_bets, :nba_playoff_matchup_id
   end
 
   def self.down
-    remove_index :nba_playoff_bets, :index_nba_playoff_bets_on_year_and_round_and_position
+    remove_index :nba_playoff_bets, {:name => "nba_playoff_bets_year_round_position_user"}
     remove_column :nba_playoff_bets, :round
     remove_column :nba_playoff_bets, :position
     add_column :nba_playoff_bets, :nba_playoff_matchup_id
--- db/migrate/20130530000000_fix_index_nba_playoff_bets.rb
+++ db/migrate/20130530000000_fix_index_nba_playoff_bets.rb
@@ -1,12 +1,13 @@
 class FixIndexNbaPlayoffBets < ActiveRecord::Migration
   def self.up
-    remove_index :nba_playoff_bets, :year_and_round_and_position
+    remove_index :nba_playoff_bets, {:name => "nba_playoff_bets_year_round_position"}
     add_index :nba_playoff_bets, [:year, :round, :position, :user_id],
         {:unique => true, :name => "nba_playoff_bets_year_round_position_user"}
   end
 
   def self.down
     remove_index :nba_playoff_bets, {:name => "nba_playoff_bets_year_round_position_user"}
-    add_index :nba_playoff_bets, [:year, :round, :position], :unique => true
+    add_index :nba_playoff_bets, [:year, :round, :position],
+        {:unique => true, :name => "nba_playoff_bets_year_round_position"}
   end
 end
